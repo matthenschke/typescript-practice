a534966eea55971366662cd37347ed5aeac9ec0c
Matthew Henschke <matthewhenschke98@gmail.com>
2020-03-22 19:50:37 -0400

    went over any types, now done with core types

---
+++ objects-enums-any.ts
@@ -0,0 +1,36 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // tuple
+// } = {
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+// enum
+enum Role {
+  ADMIN = 5, // 0
+  READ_ONLY = "READ_ONLY", // 1
+  AUTHOR = 1 // 2
+}
+const person = {
+  name: "Matt",
+  age: 21,
+  hobbies: ["coding", "sports"], // regular arr
+  role: Role.AUTHOR
+};
+
+// person.role.push("admin"); // push is allowed for tuples
+let favoriteActivities: any[]; // avoid any when possible
+favoriteActivities = ["sports", 5, true];
+console.log(person.name);
+console.log(person.role);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("author");
+}
